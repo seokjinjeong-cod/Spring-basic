a4987038be99daa03adcace993a732b7835f4dfd
seokjinjeong-cod <jsj961@naver.com>
2021-07-06 13:37:25 +0900

    commit

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -2,6 +2,7 @@ package hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class HelloSpringApplication {
